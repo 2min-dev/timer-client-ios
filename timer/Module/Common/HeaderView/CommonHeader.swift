ed72eaac3ad859f26a8ee53725765008d9990cda
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-08-06 17:22:32 +0900

    Create header views of twice type
    - Create common header view that has back button & several buttons
    - Create confirm header view that has cancel & confirm buttons

--- timer/Module/Common/HeaderView/CommonHeader.swift
+++ timer/Module/Common/HeaderView/CommonHeader.swift
@@ -7,27 +7,154 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class CommonHeader: UIView {
+    enum ButtonType: Int {
+        case back = 0
+        case home
+        case setting
+        case history
+        case search
+        case bookmark
+        case share
+        
+        var button: UIButton {
+            let button = UIButton()
+            button.tag = self.rawValue
+            
+            switch self {
+            case .back:
+                button.setImage(UIImage(named: "btn_back"), for: .normal)
+            case .home:
+                button.setImage(UIImage(named: "btn_home"), for: .normal)
+            case .setting:
+                button.setImage(UIImage(named: "btn_setting"), for: .normal)
+            case .history:
+                button.setImage(UIImage(named: "btn_history"), for: .normal)
+            case .search:
+                button.setImage(UIImage(named: "btn_search"), for: .normal)
+            case .bookmark:
+                button.setImage(UIImage(named: "btn_bookmark_off"), for: .normal)
+                button.setImage(UIImage(named: "btn_bookmark_on"), for: .selected)
+            case .share:
+                button.setImage(UIImage(named: "btn_share"), for: .normal)
+            }
+            
+            return button
+        }
+    }
+    
     // MARK: - view properties
+    let backButton: UIButton = {
+        let view = ButtonType.back.button
+        return view
+    }()
+    
+    let titleLabel: UILabel = {
+        let view = UILabel()
+        view.font = Constants.Font.ExtraBold.withSize(18.adjust())
+        view.textColor = Constants.Color.codGray
+        return view
+    }()
+    
+    private lazy var buttonStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.spacing = 5.adjust()
+        return view
+    }()
     
     // MARK: - properties
+    var title: String? {
+        set { titleLabel.text = newValue }
+        get { return titleLabel.text }
+    }
+    var buttonTypes: [ButtonType] = [] {
+        didSet {
+            // Remove all arranged subviews
+            buttonStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+            // Add all header buttons
+            buttonTypes.forEach {
+                let button = $0.button
+                button.addTarget(self, action: #selector(touchButton(sender:)), for: .touchUpInside)
+                
+                buttonStackView.addArrangedSubview(button)
+                button.snp.makeConstraints { make in
+                    make.width.equalTo(36.adjust())
+                }
+            }
+        }
+    }
+    var buttons: [UIButton] {
+        return buttonStackView.arrangedSubviews.compactMap { $0 as? UIButton }
+    }
+    
+    var isBackButtonHidden: Bool = false {
+        didSet { backButton.isHidden = isBackButtonHidden }
+    }
+    
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: superview?.bounds.width ?? 0, height: 56.adjust())
+        return CGSize(width: UIScreen.main.bounds.width, height: 75.adjust())
     }
     
     // MARK: - constructor
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = Constants.Color.white
+        
+        // Set consraint of subviews
+        addAutolayoutSubviews([backButton, titleLabel, buttonStackView])
+        backButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(10.adjust())
+            make.centerY.equalToSuperview()
+            make.width.equalTo(36.adjust())
+            make.height.equalTo(backButton.snp.width)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(backButton.snp.trailing).offset(34.adjust()).priority(999)
+            make.trailing.equalTo(buttonStackView.snp.leading).offset(-5.adjust())
+            make.centerY.equalToSuperview()
+        }
+        
+        buttonStackView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(10.adjust())
+            make.centerY.equalToSuperview()
+            // Set minimum stack view size if arranged views are empty
+            make.width.equalTo(36).priority(999)
+            make.height.equalTo(36).priority(999)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - lifecycle
-    override func layoutSubviews() {
-        layer.border(edges: [.bottom], width: 1.adjust(), color: Constants.Color.silver)
+    // MARK: - selector
+    /// Animate key pad dumping when touched
+    @objc private func touchButton(sender: UIButton) {
+        let animation = CAKeyframeAnimation(keyPath: "transform.scale")
+        animation.values = [1, 1.2, 1]
+        animation.keyTimes = [0, 0.5, 1.0]
+        animation.duration = 0.2
+
+        sender.layer.add(animation, forKey: "touch")
+    }
+}
+
+extension Reactive where Base: CommonHeader {
+    var tap: ControlEvent<Base.ButtonType> {
+        var tapButtons = base.buttons.map { button in
+            button.rx.tap
+                .flatMap { Observable<Base.ButtonType>.just(Base.ButtonType(rawValue: button.tag)!) }
+        }
+        tapButtons.append(
+            base.backButton.rx.tap
+                .flatMap { Observable<Base.ButtonType>.just(Base.ButtonType(rawValue: self.base.backButton.tag)!) })
+        
+        let source: Observable<Base.ButtonType> = .merge(tapButtons)
+        return ControlEvent(events: source)
     }
 }
--- timer/Module/Common/HeaderView/ConfirmHeader.swift
+++ timer/Module/Common/HeaderView/ConfirmHeader.swift
@@ -7,27 +7,80 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ConfirmHeader: UIView {
     // MARK: - view properties
+    let cancelButton: UIButton = {
+        let view = UIButton()
+        view.setTitle("header_button_cancel".localized, for: .normal)
+        view.setTitleColor(Constants.Color.codGray, for: .normal)
+        view.titleLabel?.font = Constants.Font.Regular.withSize(15.adjust())
+        return view
+    }()
+    
+    let titleLabel: UILabel = {
+        let view = UILabel()
+        view.textAlignment = .center
+        view.font = Constants.Font.ExtraBold.withSize(18.adjust())
+        view.textColor = Constants.Color.codGray
+        return view
+    }()
+    
+    let confirmButton: UIButton = {
+        let view = UIButton()
+        view.setTitle("header_button_confirm".localized, for: .normal)
+        view.setTitleColor(Constants.Color.codGray, for: .normal)
+        view.titleLabel?.font = Constants.Font.Regular.withSize(15.adjust())
+        return view
+    }()
+    
+    // MARK: - properties
+    var title: String? {
+        set { titleLabel.text = newValue }
+        get { return titleLabel.text }
+    }
     
     // MARK: - properties
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: superview?.bounds.width ?? 0, height: 56.adjust())
+        return CGSize(width: UIScreen.main.bounds.width, height: 75.adjust())
     }
     
     // MARK: - constructor
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = Constants.Color.white
+        
+        // Set constraint of subviews
+        addAutolayoutSubviews([cancelButton, titleLabel, confirmButton])
+        cancelButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20.adjust())
+            make.centerY.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(200.adjust())
+        }
+        
+        confirmButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(20.adjust())
+            make.centerY.equalToSuperview()
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+extension Reactive where Base: ConfirmHeader {
+    var cancel: ControlEvent<Void> {
+        return ControlEvent(events: base.cancelButton.rx.tap)
+    }
     
-    // MARK: - lifecycle
-    override func layoutSubviews() {
-        layer.border(edges: [.bottom], width: 1.adjust(), color: Constants.Color.silver)
+    var confirm: ControlEvent<Void> {
+        return ControlEvent(events: base.confirmButton.rx.tap)
     }
 }
