932f43916ed71f597296c05567d25d8389a55476
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-08-07 00:01:18 +0900

    Create timer set detail view layout
    - Add localizable strings for using time set detail

--- timer/Module/TimeSetDetail/TimeSetDetailView.swift
+++ timer/Module/TimeSetDetail/TimeSetDetailView.swift
@@ -16,8 +16,246 @@ class TimeSetDetailView: UIView {
         return view
     }()
     
-    let root: UIView = {
+    let titleLabel: UILabel = {
+        let view = UILabel()
+        // MARK: Sample text, remove it
+        view.text = "내 타임셋"
+        view.font = Constants.Font.ExtraBold.withSize(24.adjust())
+        view.textColor = Constants.Color.codGray
+        return view
+    }()
+    
+    private let sumOfTimersTitleLabel: UILabel = {
+        let view = UILabel()
+        view.text = "time_set_sum_of_all_timers_full_title".localized
+        view.font = Constants.Font.Regular.withSize(12.adjust())
+        view.textColor = Constants.Color.codGray
+        return view
+    }()
+    
+    let sumOfTimersLabel: UILabel = {
+        let view = UILabel()
+        // MARK: Sample text, remove it
+        view.text = "00:01:00"
+        view.font = Constants.Font.Bold.withSize(12.adjust())
+        view.textColor = Constants.Color.codGray
+        return view
+    }()
+    
+    private let endTimeTitleLabel: UILabel = {
+        let view = UILabel()
+        view.text = "time_set_expected_time_full_title".localized
+        view.font = Constants.Font.Regular.withSize(12.adjust())
+        view.textColor = Constants.Color.codGray
+        return view
+    }()
+    
+    let endTimeLabel: UILabel = {
+        let view = UILabel()
+        // MARK: Sample text, remove it
+        view.text = "9:42 AM"
+        view.font = Constants.Font.Bold.withSize(12.adjust())
+        view.textColor = Constants.Color.codGray
+        return view
+    }()
+    
+    let loopButton: UIButton = {
+        let view = UIButton()
+        view.setImage(UIImage(named: "btn_repeat_off"), for: .normal)
+        view.setImage(UIImage(named: "btn_repeat_on"), for: .selected)
+        return view
+    }()
+    
+    let plus1MinButton: UIButton = {
+        let view = UIButton()
+        view.setImage(UIImage(named: "btn_plus_1min_disable"), for: .normal)
+        return view
+    }()
+    
+    private lazy var timeSetInfoView: UIView = {
         let view = UIView()
+        
+        let divider = UIView()
+        divider.backgroundColor = Constants.Color.codGray
+        
+        // Set constraint of subviews
+        view.addAutolayoutSubviews([sumOfTimersTitleLabel, sumOfTimersLabel, endTimeTitleLabel, endTimeLabel, plus1MinButton, loopButton, divider])
+        sumOfTimersTitleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(11.adjust())
+            make.leading.equalToSuperview()
+            make.trailing.equalTo(endTimeTitleLabel)
+        }
+        
+        sumOfTimersLabel.snp.makeConstraints { make in
+            make.top.equalTo(sumOfTimersTitleLabel)
+            make.leading.equalTo(sumOfTimersTitleLabel.snp.trailing).offset(10.adjust())
+            make.bottom.equalTo(sumOfTimersTitleLabel)
+        }
+        
+        endTimeTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(sumOfTimersTitleLabel.snp.bottom).offset(10.adjust())
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview().inset(16.adjust())
+        }
+        
+        endTimeLabel.snp.makeConstraints { make in
+            make.top.equalTo(endTimeTitleLabel)
+            make.leading.equalTo(endTimeTitleLabel.snp.trailing).offset(10.adjust())
+            make.bottom.equalTo(endTimeTitleLabel)
+        }
+        
+        plus1MinButton.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.trailing.equalToSuperview().inset(10.adjust())
+            make.width.equalTo(36.adjust())
+            make.height.equalTo(plus1MinButton.snp.width)
+        }
+
+        loopButton.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.trailing.equalTo(plus1MinButton.snp.leading).offset(-10.adjust())
+            make.width.equalTo(36.adjust())
+            make.height.equalTo(plus1MinButton.snp.width)
+        }
+
+        divider.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalTo(1)
+        }
+        
+        return view
+    }()
+    
+    let timerBadgeCollectionView: TimerBadgeCollectionView = {
+        let view = TimerBadgeCollectionView()
+//        view.setContentHuggingPriority(.required, for: .vertical)
+        return view
+    }()
+    
+    private let alarmIconImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "icon_sound")
+        return view
+    }()
+    
+    let alarmLabel: UILabel = {
+        let view = UILabel()
+        // MARK: Sample text, remove it
+        view.text = "기본음"
+        view.font = Constants.Font.Regular.withSize(12.adjust())
+        view.textColor = Constants.Color.codGray
+        return view
+    }()
+    
+    private lazy var alarmView: UIView = {
+        let view = UIView()
+        
+        // Set constraint of subviews
+        view.addAutolayoutSubviews([alarmIconImageView, alarmLabel])
+        alarmIconImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.width.equalTo(36.adjust())
+            make.height.equalTo(alarmIconImageView.snp.width)
+        }
+        
+        alarmLabel.snp.makeConstraints { make in
+            make.leading.equalTo(alarmIconImageView.snp.trailing).offset(5.adjust())
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+        return view
+    }()
+    
+    private let commentIconImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "icon_comment")
+        return view
+    }()
+    
+    let commentTextView: UITextView = {
+        let view = UITextView()
+        // MARK: Sample text, remove it
+        view.text = "코멘트\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na"
+        view.font = Constants.Font.Regular.withSize(12.adjust())
+        view.textColor = Constants.Color.codGray
+        // Remove padding
+        view.textContainer.lineFragmentPadding = 0
+        view.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        return view
+    }()
+    
+    private lazy var commentView: UIView = {
+        let view = UIView()
+        
+        // Set constraint of subviews
+        view.addAutolayoutSubviews([commentIconImageView, commentTextView])
+        commentIconImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.width.equalTo(36.adjust())
+            make.height.equalTo(commentIconImageView.snp.width)
+        }
+        
+        commentTextView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(11.33.adjust())
+            make.leading.equalTo(commentIconImageView.snp.trailing).offset(5.adjust())
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+        return view
+    }()
+    
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        
+        // Set constraint of subviews
+        view.addAutolayoutSubviews([titleLabel, timeSetInfoView, timerBadgeCollectionView, alarmView, commentView])
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview().inset(60.adjust())
+            make.trailing.equalToSuperview().inset(10.adjust())
+        }
+
+        timeSetInfoView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(185.adjust())
+            make.leading.equalTo(titleLabel)
+            make.trailing.equalToSuperview()
+        }
+
+        timerBadgeCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(timeSetInfoView.snp.bottom).offset(8.adjust())
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+
+        alarmView.snp.makeConstraints { make in
+            make.top.equalTo(timerBadgeCollectionView.snp.bottom).offset(14.adjust())
+            make.leading.equalToSuperview().inset(50.adjust())
+            make.trailing.equalToSuperview().inset(10.adjust())
+        }
+
+        commentView.snp.makeConstraints { make in
+            make.top.equalTo(alarmView.snp.bottom).inset(9.adjust())
+            make.leading.equalToSuperview().inset(50.adjust())
+            make.trailing.equalToSuperview().inset(10.adjust())
+            make.bottom.equalToSuperview()
+        }
+        
+        return view
+    }()
+    
+    let footerView: Footer = {
+        let view = Footer()
+        view.buttons = [
+            FooterButton(title: "footer_button_edit".localized, type: .normal),
+            FooterButton(title: "footer_button_start".localized, type: .highlight)
+        ]
         return view
     }()
     
@@ -26,7 +264,7 @@ class TimeSetDetailView: UIView {
         super.init(frame: frame)
         backgroundColor = Constants.Color.white
         
-        addAutolayoutSubviews([headerView, root])
+        addAutolayoutSubviews([headerView, contentView, footerView])
         headerView.snp.makeConstraints { make in
             if #available(iOS 11.0, *) {
                 make.top.equalTo(safeAreaLayoutGuide)
@@ -37,8 +275,14 @@ class TimeSetDetailView: UIView {
             make.trailing.equalToSuperview()
         }
         
-        root.snp.makeConstraints { make in
-            make.top.equalTo(headerView.snp.bottom)
+        contentView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom).offset(35.adjust())
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(footerView.snp.top).offset(-10.adjust())
+        }
+        
+        footerView.snp.makeConstraints { make in
             make.leading.equalToSuperview()
             make.trailing.equalToSuperview()
             make.bottom.equalToSuperview()
