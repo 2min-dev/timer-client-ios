a45d1d0150ca05c6baa63626b518a577da86624a
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-10-29 02:27:37 +0900

    Update timer models
    - Update to separate timer info model to `TimerItem` and `StopWatchItem` implemented Recordable
    - Create `Recordable` and `Alertable` protocol normalized from `TimerInfo`
    - Rename `TMTimer` to `JSTimer` and refactoring all process

---
+++ timer/Source/Model/Data/StopWatchItem.swift
@@ -0,0 +1,38 @@
+//
+//  StopWatchItem.swift
+//  timer
+//
+//  Created by JSilver on 2019/10/29.
+//  Copyright © 2019 Jeong Jin Eun. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class StopwatchItem: Object, Codable, NSCopying, Recordable {
+    @objc dynamic var current: TimeInterval = 0
+    @objc dynamic var end: TimeInterval = 0
+    var isEnded: Bool = false
+    
+    // MARK: - constructor
+    convenience init(current: TimeInterval, end: TimeInterval) {
+        self.init()
+        self.current = current
+        self.end = end
+    }
+    
+    // MARK: - public method
+    func copy(with zone: NSZone? = nil) -> Any {
+        return StopwatchItem(current: current, end: end)
+    }
+    
+    func consume(time: TimeInterval) -> TimeInterval {
+        current += time
+        return 0
+    }
+    
+    func reset() {
+        current = 0
+        end = 0
+    }
+}
---
+++ timer/Source/Model/Protocol/Recordable.swift
@@ -0,0 +1,27 @@
+//
+//  Recordable.swift
+//  timer
+//
+//  Created by JSilver on 2019/10/29.
+//  Copyright © 2019 Jeong Jin Eun. All rights reserved.
+//
+
+import Foundation
+
+protocol Recordable {
+    /// Current time interval of the timer
+    var current: TimeInterval { get }
+    /// Target end time interval of the timer
+    var end: TimeInterval { get }
+    /// Is timer process ended
+    var isEnded: Bool { get }
+    
+    /// Consume time to timer
+    /// - parameter time: the time to consume
+    /// - returns: remaining time after consume
+    @discardableResult
+    func consume(time: TimeInterval) -> TimeInterval
+    
+    /// Reset time data
+    func reset()
+}
