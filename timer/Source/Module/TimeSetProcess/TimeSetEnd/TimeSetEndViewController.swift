45b04d4f4034ea2504794c90d375047fecd2d1cf
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-11-06 00:05:30 +0900

    fix($iOS-1572610380000): fix time set memo excess view logic

--- timer/Source/Module/TimeSetProcess/TimeSetEnd/TimeSetEndViewController.swift
+++ timer/Source/Module/TimeSetProcess/TimeSetEnd/TimeSetEndViewController.swift
@@ -12,7 +12,7 @@ import ReactorKit
 
 class TimeSetEndViewController: BaseHeaderViewController, View {
     // MARK: - constants
-    private let MAX_MEMO_LENGTH: Int = 1000
+    private static let MAX_MEMO_LENGTH: Int = 1000
     
     // MARK: - view properties
     private var timeSetEndView: TimeSetEndView { return view as! TimeSetEndView }
@@ -33,6 +33,8 @@ class TimeSetEndViewController: BaseHeaderViewController, View {
     // MARK: - properties
     var coordinator: TimeSetEndViewCoordinator
     
+    var isExceeded: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+    
     // MARK: - constructor
     init(coordinator: TimeSetEndViewCoordinator) {
         self.coordinator = coordinator
@@ -48,6 +50,48 @@ class TimeSetEndViewController: BaseHeaderViewController, View {
         view = TimeSetEndView()
     }
     
+    override func bind() {
+        super.bind()
+        
+        memoTextView.rx.text
+            .map { !$0!.isEmpty }
+            .bind(to: memoHintLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        memoTextView.rx.text
+            .orEmpty
+            .filter { $0.lengthOfBytes(using: .utf16) > Self.MAX_MEMO_LENGTH }
+            .do(onNext: { [weak self] _ in self?.isExceeded.accept(true) })
+            .map { String($0.dropLast()) }
+            .bind(to: memoTextView.rx.text)
+            .disposed(by: disposeBag)
+        
+        // Memo length
+        Observable.combineLatest(
+            memoTextView.rx.text
+                .orEmpty
+                .map { $0.lengthOfBytes(using: .utf16) }
+                .distinctUntilChanged(),
+            isExceeded.distinctUntilChanged())
+            .compactMap { [weak self] in self?.getMemoLengthAttributedString(length: $0, isExceeded: $1) }
+            .bind(to: memoLengthLabel.rx.attributedText)
+            .disposed(by: disposeBag)
+        
+        // Exceeded memo
+        isExceeded
+            .map { !$0 }
+            .distinctUntilChanged()
+            .bind(to: memoExcessLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        isExceeded
+            .filter { $0 }
+            .debounce(.seconds(3), scheduler: MainScheduler.instance)
+            .map { !$0 }
+            .bind(to: isExceeded)
+            .disposed(by: disposeBag)
+    }
+    
     // MARK: - bine
     func bind(reactor: TimeSetEndViewReactor) {
         // MARK: action
@@ -59,19 +103,6 @@ class TimeSetEndViewController: BaseHeaderViewController, View {
         rx.viewWillDisappear
             .map { Reactor.Action.viewWillDisappear }
             .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-        
-        memoTextView.rx.text
-            .map { !$0!.isEmpty }
-            .bind(to: memoHintLabel.rx.isHidden)
-            .disposed(by: disposeBag)
-        
-        memoTextView.rx.text
-            .orEmpty
-            .map { ($0, $0.lengthOfBytes(using: .utf16)) }
-            .filter { [weak self] in $0.1 > (self?.MAX_MEMO_LENGTH ?? 0) }
-            .map { String($0.0.dropLast()) }
-            .bind(to: memoTextView.rx.text)
             .disposed(by: disposeBag)
         
         memoTextView.rx.text
@@ -115,45 +146,15 @@ class TimeSetEndViewController: BaseHeaderViewController, View {
             .filter { [weak self] in self?.memoTextView.text != $0 }
             .bind(to: memoTextView.rx.text)
             .disposed(by: disposeBag)
-        
-        // Memo length
-        let lengthOfBytes = reactor.state
-            .map { $0.memo }
-            .map { $0.lengthOfBytes(using: .utf16) }
-            .distinctUntilChanged()
-            .share(replay: 1)
-        
-        let isMemoExceeded = lengthOfBytes
-            .map { [weak self] in $0 >= self?.MAX_MEMO_LENGTH ?? 0 }
-            .distinctUntilChanged()
-            .share(replay: 1)
-        
-        // Length text
-        Observable.combineLatest(lengthOfBytes, isMemoExceeded)
-            .compactMap { [weak self] in self?.getMemoLengthAttributedString(length: $0, isExcess: $1) }
-            .bind(to: memoLengthLabel.rx.attributedText)
-            .disposed(by: disposeBag)
-        
-        // Exceeded info
-        isMemoExceeded
-            .map { !$0 }
-            .bind(to: memoExcessLabel.rx.isHidden)
-            .disposed(by: disposeBag)
-        
-        isMemoExceeded
-            .filter { $0 }
-            .debounce(.seconds(3), scheduler: MainScheduler.instance)
-            .bind(to: memoExcessLabel.rx.isHidden)
-            .disposed(by: disposeBag)
     }
     
     // MARK: - state method
     /// Get memo length attributed string
-    private func getMemoLengthAttributedString(length: Int, isExcess: Bool) -> NSAttributedString {
-        let lengthString = String(format: "time_set_memo_bytes_format".localized, length, MAX_MEMO_LENGTH)
+    private func getMemoLengthAttributedString(length: Int, isExceeded: Bool) -> NSAttributedString {
+        let lengthString = String(format: "time_set_memo_bytes_format".localized, length, Self.MAX_MEMO_LENGTH)
         let attributedString = NSMutableAttributedString(string: lengthString)
         
-        if isExcess {
+        if isExceeded {
             // Highlight length text
             let range = NSString(string: lengthString).range(of: String(length))
             attributedString.addAttribute(.foregroundColor, value: Constants.Color.carnation, range: range)
--- timer/Source/Module/TimeSetProcess/TimeSetMemo/TimeSetMemoViewController.swift
+++ timer/Source/Module/TimeSetProcess/TimeSetMemo/TimeSetMemoViewController.swift
@@ -12,7 +12,7 @@ import ReactorKit
 
 class TimeSetMemoViewController: BaseHeaderViewController, View {
     // MARK: - constants
-    private let MAX_MEMO_LENGTH: Int = 1000
+    private static let MAX_MEMO_LENGTH: Int = 1000
     
     // MARK: - view properties
     private var timeSetMemoView: TimeSetMemoView { return view as! TimeSetMemoView }
@@ -26,6 +26,8 @@ class TimeSetMemoViewController: BaseHeaderViewController, View {
     
     // MARK: - properties
     var coordinator: TimeSetMemoViewCoordinator
+    
+    var isExceeded: BehaviorRelay<Bool> = BehaviorRelay(value: false)
     
     // MARK: - constructor
     init(coordinator: TimeSetMemoViewCoordinator) {
@@ -46,13 +48,8 @@ class TimeSetMemoViewController: BaseHeaderViewController, View {
         super.viewDidLoad()
     }
     
-    // MARK: - bine
-    func bind(reactor: TimeSetMemoViewReactor) {
-        // MARK: action
-        rx.viewDidAppear
-            .take(1)
-            .subscribe(onNext: { [weak self] in self?.memoTextView.becomeFirstResponder() })
-            .disposed(by: disposeBag)
+    override func bind() {
+        super.bind()
         
         memoTextView.rx.text
             .map { !$0!.isEmpty }
@@ -61,10 +58,44 @@ class TimeSetMemoViewController: BaseHeaderViewController, View {
         
         memoTextView.rx.text
             .orEmpty
-            .map { ($0, $0.lengthOfBytes(using: .utf16)) }
-            .filter { [weak self] in $0.1 > (self?.MAX_MEMO_LENGTH ?? 0) }
-            .map { String($0.0.dropLast()) }
+            .filter { $0.lengthOfBytes(using: .utf16) > Self.MAX_MEMO_LENGTH }
+            .do(onNext: { [weak self] _ in self?.isExceeded.accept(true) })
+            .map { String($0.dropLast()) }
             .bind(to: memoTextView.rx.text)
+            .disposed(by: disposeBag)
+        
+        // Memo length
+        Observable.combineLatest(
+            memoTextView.rx.text
+                .orEmpty
+                .map { $0.lengthOfBytes(using: .utf16) }
+                .distinctUntilChanged(),
+            isExceeded.distinctUntilChanged())
+            .compactMap { [weak self] in self?.getMemoLengthAttributedString(length: $0, isExceeded: $1) }
+            .bind(to: memoLengthLabel.rx.attributedText)
+            .disposed(by: disposeBag)
+        
+        // Exceeded memo
+        isExceeded
+            .map { !$0 }
+            .distinctUntilChanged()
+            .bind(to: memoExcessLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        isExceeded
+            .filter { $0 }
+            .debounce(.seconds(3), scheduler: MainScheduler.instance)
+            .map { !$0 }
+            .bind(to: isExceeded)
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: - bine
+    func bind(reactor: TimeSetMemoViewReactor) {
+        // MARK: action
+        rx.viewDidAppear
+            .take(1)
+            .subscribe(onNext: { [weak self] in self?.memoTextView.becomeFirstResponder() })
             .disposed(by: disposeBag)
         
         memoTextView.rx.text
@@ -81,46 +112,16 @@ class TimeSetMemoViewController: BaseHeaderViewController, View {
             .filter { [weak self] in self?.memoTextView.text != $0 }
             .bind(to: memoTextView.rx.text)
             .disposed(by: disposeBag)
-        
-        // Memo length
-        let lengthOfBytes = reactor.state
-            .map { $0.memo }
-            .map { $0.lengthOfBytes(using: .utf16) }
-            .distinctUntilChanged()
-            .share(replay: 1)
-        
-        let isMemoExceeded = lengthOfBytes
-            .map { [weak self] in $0 >= self?.MAX_MEMO_LENGTH ?? 0 }
-            .distinctUntilChanged()
-            .share(replay: 1)
-        
-        // Length text
-        Observable.combineLatest(lengthOfBytes, isMemoExceeded)
-            .compactMap { [weak self] in self?.getMemoLengthAttributedString(length: $0, isExcess: $1) }
-            .bind(to: memoLengthLabel.rx.attributedText)
-            .disposed(by: disposeBag)
-        
-        // Exceeded info
-        isMemoExceeded
-            .map { !$0 }
-            .bind(to: memoExcessLabel.rx.isHidden)
-            .disposed(by: disposeBag)
-        
-        isMemoExceeded
-            .filter { $0 }
-            .debounce(.seconds(3), scheduler: MainScheduler.instance)
-            .bind(to: memoExcessLabel.rx.isHidden)
-            .disposed(by: disposeBag)
     }
     
     // MARK: - action method
     // MARK: - state method
     /// Get memo length attributed string
-    private func getMemoLengthAttributedString(length: Int, isExcess: Bool) -> NSAttributedString {
-        let lengthString = String(format: "time_set_memo_bytes_format".localized, length, MAX_MEMO_LENGTH)
+    private func getMemoLengthAttributedString(length: Int, isExceeded: Bool) -> NSAttributedString {
+        let lengthString = String(format: "time_set_memo_bytes_format".localized, length, Self.MAX_MEMO_LENGTH)
         let attributedString = NSMutableAttributedString(string: lengthString)
         
-        if isExcess {
+        if isExceeded {
             // Highlight length text
             let range = NSString(string: lengthString).range(of: String(length))
             attributedString.addAttribute(.foregroundColor, value: Constants.Color.carnation, range: range)
