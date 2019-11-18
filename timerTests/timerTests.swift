5c2f3ad9ccb561c14f967fd4767ea97d9c43a88b
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-11-18 16:49:18 +0900

    feat(): create test targets

--- timerTests/timerTests.swift
+++ timerTests/timerTests.swift
@@ -1,14 +1,14 @@
 //
-//  test.swift
-//  test
+//  timerTests.swift
+//  timerTests
 //
-//  Created by JSilver on 28/04/2019.
+//  Created by JSilver on 2019/11/18.
 //  Copyright © 2019 Jeong Jin Eun. All rights reserved.
 //
 
 import XCTest
 
-class TimeSetServiceTest: XCTestCase {
+class timerTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,4 +22,12 @@ class TimeSetServiceTest: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
 }
