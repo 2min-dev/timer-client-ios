dc3f143ac738bb03f384b99854b55ea6fa76227f
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-11-25 04:39:11 +0900

    feat($1574065140000): update localizable strings to support english
    - Change date format of english locale
    - Update time set edit view layout values
    - Update time set process repetition strings for plural

--- timer/Source/Module/Productivity/ProductivityView.swift
+++ timer/Source/Module/Productivity/ProductivityView.swift
@@ -26,7 +26,7 @@ class ProductivityView: UIView {
     
     let allTimeLabel: UILabel = {
         let view = UILabel()
-        view.font = Constants.Font.Regular.withSize(10.adjust())
+        view.font = Constants.Font.Regular.withSize(12.adjust())
         view.textColor = Constants.Color.codGray
         view.textAlignment = .center
         return view
@@ -34,7 +34,7 @@ class ProductivityView: UIView {
     
     let endOfTimeSetLabel: UILabel = {
         let view = UILabel()
-        view.font = Constants.Font.Regular.withSize(10.adjust())
+        view.font = Constants.Font.Regular.withSize(12.adjust())
         view.textColor = Constants.Color.codGray
         view.textAlignment = .center
         return view
@@ -60,7 +60,7 @@ class ProductivityView: UIView {
     let keyPadView: NumberKeyPad = {
         let view = NumberKeyPad()
         view.font = Constants.Font.Regular.withSize(24.adjust())
-        view.cancelButton.titleLabel?.font = Constants.Font.Regular.withSize(20.adjust())
+        view.cancelButton.titleLabel?.font = Constants.Font.Regular.withSize(18.adjust())
         view.foregroundColor = Constants.Color.codGray
         view.cancelButton.setHidden(true)
         
